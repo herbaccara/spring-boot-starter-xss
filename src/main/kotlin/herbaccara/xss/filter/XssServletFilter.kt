3f51c7ae6170c150de2aac795ee34bccdfd1832c
이용홍 <allbegray.unimewo@gmail.com>
2023-03-16 10:22:45 +0900

    :adhesive_bandage: XssServletFilter 상속 Filter -> OncePerRequestFilter 변경

--- src/main/kotlin/herbaccara/xss/filter/XssServletFilter.kt
+++ src/main/kotlin/herbaccara/xss/filter/XssServletFilter.kt
@@ -2,34 +2,35 @@ package herbaccara.xss.filter
 
 import herbaccara.xss.XssHttpServletRequestWrapper
 import herbaccara.xss.annotation.XssFilter
-import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.core.annotation.AnnotatedElementUtils
+import org.springframework.web.filter.OncePerRequestFilter
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping
 
 class XssServletFilter(
     private val requestMappingHandlerMapping: RequestMappingHandlerMapping
-) : Filter {
+) : OncePerRequestFilter() {
 
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        if (request is HttpServletRequest) {
-            val handlerExecutionChain = requestMappingHandlerMapping.getHandler(request)
-            if (handlerExecutionChain != null) {
-                val handler = handlerExecutionChain.handler
-                if (handler is HandlerMethod) {
-                    val xssFilter = handler.getMethodAnnotation(XssFilter::class.java)
-                        ?: AnnotatedElementUtils.findMergedAnnotation(handler.beanType, XssFilter::class.java)
-                    if (xssFilter != null) {
-                        chain.doFilter(XssHttpServletRequestWrapper(request, xssFilter.level), response)
-                        return
-                    }
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val handlerExecutionChain = requestMappingHandlerMapping.getHandler(request)
+        if (handlerExecutionChain != null) {
+            val handler = handlerExecutionChain.handler
+            if (handler is HandlerMethod) {
+                val xssFilter = handler.getMethodAnnotation(XssFilter::class.java)
+                    ?: AnnotatedElementUtils.findMergedAnnotation(handler.beanType, XssFilter::class.java)
+                if (xssFilter != null) {
+                    filterChain.doFilter(XssHttpServletRequestWrapper(request, xssFilter.level), response)
+                    return
                 }
             }
         }
-        chain.doFilter(request, response)
+        filterChain.doFilter(request, response)
     }
 }
